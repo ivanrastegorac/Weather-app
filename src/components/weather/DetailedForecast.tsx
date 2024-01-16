c2225eca569eef278038f9be50533bd597d7025b
irastegorac <ivan.rastegorac@ag04.com>
2024-01-16 15:17:17 +0100

    Refactor DetailForecast component

--- src/components/weather/DetailedForecast.tsx
+++ src/components/weather/DetailedForecast.tsx
@@ -5,25 +5,42 @@ import { Temperature, fetchTemperature } from "../../services/weatherService";
 
 const DetailedForecast: React.FC = () => {
   const { cityName, day } = useParams<{ cityName: string; day: string }>();
-  const [selectedDayForecast, setSelectedDayForecast] =
-    useState<Temperature | null>(null);
+  const [selectedDayForecast, setSelectedDayForecast] = useState<Temperature | null>(null);
+
+  const definedHours = ["01:00", "07:00", "13:00", "19:00"];
 
    useEffect(() => {
     const fetchData = async () => {
-      try {
-        if (cityName) {
-          const response = await fetchTemperature(cityName);
-          setSelectedDayForecast(response);
-        } else {
-          console.error("City name is undefined");
+        try {
+          if (cityName) {
+            const response = await fetchTemperature(cityName);
+            setSelectedDayForecast(response);
+          } else {
+           console.error("City name is undefined");
+          }
+        } catch (error) {
+          console.error("Failed to fetch temperature data", error);
         }
-      } catch (error) {
-        console.error("Failed to fetch temperature data", error);
+      };
+
+      fetchData();
+    }, [cityName]);
+
+    const getTemperatureForHour = (hour: string): string => {
+      switch (hour) {
+        case "01:00":
+          return String(selectedDayForecast?.morn) ?? "";
+        case "07:00":
+          return String(selectedDayForecast?.day) ?? "";
+        case "13:00":
+          return String(selectedDayForecast?.eve) ?? "";
+        case "19:00":
+          return String(selectedDayForecast?.night) ?? "";
+        default:
+          return "";
       }
     };
-
-    fetchData();
-  }, [cityName]);
+    
 
   return (
     <ForecastContainer>
@@ -41,7 +58,7 @@ const DetailedForecast: React.FC = () => {
       </tr>
     </thead>
     <tbody>
-      {["01:00", "07:00", "13:00", "19:00"].map((hour, hourIndex) => (
+      {definedHours.map((hour, hourIndex) => (
         <tr key={hourIndex}>
           <td>{hour}</td>
 
@@ -49,18 +66,7 @@ const DetailedForecast: React.FC = () => {
             <WeatherImage weatherIcon={selectedDayForecast.weatherIcon} />
           </ForecastIcon>
 
-          <td>
-            {hour === "01:00"
-              ? selectedDayForecast.morn
-              : hour === "07:00"
-              ? selectedDayForecast.day
-              : hour === "13:00"
-              ? selectedDayForecast.eve
-              : hour === "19:00"
-              ? selectedDayForecast.night
-              : ""}
-            °C
-          </td>
+          <td>{getTemperatureForHour(hour)}°C</td>
         </tr>
       ))}
     </tbody>
